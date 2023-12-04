d5b26365aa0d889f25db65b28f203188351a19b0
michaeltiat <tranthanhhai5a6@gmail.com>
2023-12-04 13:10:25 +0700

    cell3

--- main.py
+++ main.py
@@ -17,12 +17,16 @@ def main(cfg):
     algorithm = instantiate(cfg.algorithm)
 
     default_chromo = instantiate(cfg.chromosome, chromo=None)
-    chromosomes = [default_chromo] * (cfg.searcher.n_cells - 1)
+    chromosomes = [default_chromo] * (cfg.searcher.n_cells - 2)
     chromo1 = instantiate(
         cfg.chromosome,
         chromo=[1, 1, 1, 1, 1, 1, 1, 9, 9, 1, 1, 1]
     )
-    chromosomes = chromosomes + [chromo1]
+    chromo2 = instantiate(
+        cfg.chromosome,
+        chromo=[2, 1, 1, 1, 1, 1, 1, 10, 2, 1, 1, 1]
+    )
+    chromosomes = chromosomes + [chromo2, chromo1]
     print("search")
     searcher.search(
         cfg=cfg,
--- scripts/visualize_brute_force.py
+++ scripts/visualize_brute_force.py
@@ -9,7 +9,7 @@ from zebanas.genetic.population import Population
 from zebanas.genetic.utils import fast_non_dominated_sorting
 
 
-path = "/home/haitt/workspaces/codes/nas/zebanas/logs/2023-12-03/checkpoints"
+path = "/home/haitt/workspaces/codes/nas/zebanas/logs/2023-12-04/checkpoints"
 
 full_population = []
 for i in range(7):
@@ -34,29 +34,29 @@ front_population = Population.create(front_population)
 front_population = front_population.reshape(-1)
 front_objs = front_population.get_obj()
 
-mid_obj = np.mean(front_objs, axis=0)
+# mid_obj = np.mean(front_objs, axis=0)
 
 
-dis_matrix = np.sum(np.power(front_objs - mid_obj, 2), axis=1)
-dis_matrix = np.sqrt(dis_matrix)
-min_idx = np.argmin(dis_matrix)
+# dis_matrix = np.sum(np.power(front_objs - mid_obj, 2), axis=1)
+# dis_matrix = np.sqrt(dis_matrix)
+# min_idx = np.argmin(dis_matrix)
 
-print(min_idx)
-print(front_population[min_idx].data)
-
-# front_scores = front_objs[:, 0]
-# front_scores = (front_scores - np.min(front_scores)) / (np.max(front_scores) - np.min(front_scores))
-
-# front_latency = front_objs[:, 1]
-# front_latency = (front_latency - np.min(front_latency)) / (np.max(front_latency) - np.min(front_latency))
-
-# value = 0.7*front_scores + 0.3*front_latency
-# min_idx = np.argmin(value)
+# print(min_idx)
 # print(front_population[min_idx].data)
+
+front_scores = front_objs[:, 0]
+front_scores = (front_scores - np.min(front_scores)) / (np.max(front_scores) - np.min(front_scores))
+
+front_latency = front_objs[:, 1]
+front_latency = (front_latency - np.min(front_latency)) / (np.max(front_latency) - np.min(front_latency))
+
+value = 0.6*front_scores + 0.4*front_latency
+min_idx = np.argmin(value)
+print(front_population[min_idx].data)
 
 plt.figure()
 plt.scatter(objs[:, 0], objs[:, 1])
 plt.scatter(objs[fronts, 0], objs[fronts, 1])
-plt.scatter(mid_obj[0], mid_obj[1])
+# plt.scatter(mid_obj[0], mid_obj[1])
 plt.scatter(front_objs[min_idx, 0], front_objs[min_idx, 1])
 plt.show()
