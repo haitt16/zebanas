e789c9b481c1da87664d3605666bbc76b52c9c9e
michaeltiat <tranthanhhai5a6@gmail.com>
2024-01-08 21:55:00 +0700

    model benchmark

--- train.py
+++ train.py
@@ -1,16 +1,17 @@
 from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.trainer.trainer import Trainer
-
+import lightning.pytorch as pl
 import hydra
 from hydra.utils import instantiate
 
 from nats_bench import create
-import xautodl
 from xautodl.models import get_cell_based_tiny_net
 
 @hydra.main(version_base=None, config_path="zebanas/configs/classification", config_name="train")
 def main(cfg):
+    pl.seed_everything(777, workers=True)
+
     lr_monitor = LearningRateMonitor(logging_interval="step")
     checkpoint_callback = ModelCheckpoint(
         **cfg.callback.checkpoint
